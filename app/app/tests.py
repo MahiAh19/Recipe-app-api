a2b33afe4897dfd583ec62b18ca7b5e15cdeef63
Mahima Ahsan <mahima.ahsan19@gmail.com>
2023-03-14 09:09:35 +0600

    lint

--- app/app/tests.py
+++ app/app/tests.py
@@ -10,13 +10,13 @@ class CalcTests(SimpleTestCase):
     """ Test the calc module. """
     def test_add_numbers(self):
 
-         """ Test adding numbers together"""
-         res = calc.add(5, 6)
+        """ Test adding numbers together"""
+        res = calc.add(5, 6)
 
-         self.assertEqual(res, 11)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-         """subtract"""
-         res = calc.subtract(10, 15)
+        """subtract"""
+        res = calc.subtract(10, 15)
 
-         self.assertEqual(res, 5)
+        self.assertEqual(res, 5)
